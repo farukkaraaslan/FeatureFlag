c478a094fa63072e3369733e257898251e39ed87
cahityusufkafadar <cahityusuf@gmail.com>
2024-01-03 17:25:17 +0300

    duzenleme

--- FeatureFlag/FeatureFlag/Controllers/HomeController.cs
+++ FeatureFlag/FeatureFlag/Controllers/HomeController.cs
@@ -30,5 +30,6 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        
     }
 }
